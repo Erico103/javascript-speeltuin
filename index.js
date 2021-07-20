c69c56cccf13b62ad5c93125ab13161aa4f1534b
Erico103 <85289051+Erico103@users.noreply.github.com>
2021-07-20 12:31:24 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+let firstName = "Erico";
+let lastName = "Stuurman";
+let age = 51;
+
+
+
+
+console.log(firstName+" "+lastName)
+console.log(4 + 4)
+console.log('4'+'4')
+console.log(10-3)
+console.log(20/4)
+console.log(12%3)
+console.log(typeof age)
+
+// je kan geen variabele maken met If omdat deze al gereserveerd is
